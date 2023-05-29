3c8c2bb553cdbb1356c360d0f3ef1840e3b43889
Wesley Peters <wesleypeters@gmail.com>
2023-05-29 12:40:50 -0600

    adds DOMContentLoaded event listener and replaces p text

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", (event) => {
+    const p = document.getElementById("text");
+    p.textContent = "This is really cool!"
+});
+
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
